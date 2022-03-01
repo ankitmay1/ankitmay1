3da0e4f55ac8c9112df4fcc924ce16c6c06ddd97
ankitmay1 <100698811+ankitmay1@users.noreply.github.com>
2022-03-01 20:25:27 +0530

    Add files via upload

---
+++ q1 sheet1.py
@@ -0,0 +1,38 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+5**9
+1953125
+3//2
+1
+7//3
+2
+7/3
+2.3333333333333335
+a=20;a+=30;a%=3;print(a)
+2
+6==6
+True
+a=20;a+=30;a%=3;print(a)
+2
+true*false
+Traceback (most recent call last):
+  File "<pyshell#7>", line 1, in <module>
+    true*false
+NameError: name 'true' is not defined. Did you mean: 'True'?
+True*False
+0
+True &False
+False
+True and False
+False
+((6>3)and(7<4) or(18==3))and(9>3)
+False
+True is False
+False
+False in 'False'
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    False in 'False'
+TypeError: 'in <string>' requires string as left operand, not bool
+((True ==False) or (False >True)) and (False<=True)
+False
---
+++ q2 shet1.py
@@ -0,0 +1,7 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+s1="nice to have it"
+s2=" here "
+s3=s1+s2
+print(s3)
+nice to have it here 
